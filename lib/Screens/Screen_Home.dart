7aae836f9987106e62ae6890029bda76d0146e00
Muhsin MuhammedAli <muhsin903125@gmail.com>
2022-02-03 23:31:50 +0530

    aa

--- lib/Screens/Screen_Home.dart
+++ lib/Screens/Screen_Home.dart
@@ -7,7 +7,7 @@ class ScreenHome extends StatelessWidget {
   const ScreenHome({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    final _random = new Random();
+    //final _random = new Random();
     var imagePath = [
       'https://media.istockphoto.com/photos/millennial-male-team-leader-organize-virtual-workshop-with-employees-picture-id1300972574?s=612x612'
           'http://www.shadowsphotography.co/wp-content/uploads/2018/01/Priyanka-Balasubramanian-Siddhartha-100x100.jpg',
